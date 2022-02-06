f02f1862f83779bce6fce232ff839307705f63ad
TitoMarazG <95193204+TitoMarazG@users.noreply.github.com>
2022-02-06 19:34:04 +0100

    Update combinazioni.h

--- combinazioni.h
+++ combinazioni.h
@@ -11,12 +11,10 @@ int cartaAlta(Carta mano[]);
 int coppia(Carta mano[]);
 int doppiaCoppia(Carta mano[]);
 int tris(Carta mano[]);
-int scala(Carta mano[]);
+int scala(Carta mano[]); int trovaCarta(Carta mano[], int val); void swap(Carta * c1, Carta * c2); void sort(Carta mano[]);
 int colore(Carta mano[]);
-/*
-int full(Carta mano[]);*/
+int full(Carta mano[]);
 int poker(Carta mano[]);
-/*
-int scalaColore(Carta mano[]);*/
+int scalaColore(Carta mano[]); int trovaCartaColore(Carta mano[],int val,char seed);
 
 #endif // COMBINAZIONI_H_INCLUDED
